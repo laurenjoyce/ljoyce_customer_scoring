f61a2cf63063c7d41a4bcbabbf9f29735dda8b04
Lauren Joyce <joycelc4186@gmail.com>
2017-03-02 19:42:53 -0600

    propensity and ranking RSPEC tests

--- spec/ljoyce_customer_scoring_spec.rb
+++ spec/ljoyce_customer_scoring_spec.rb
@@ -7,15 +7,33 @@ describe LjoyceCustomerScoring do
 
   # query method returns an Analysis object
   it "returns an instance of the Analysis class" do
-    expect(LjoyceCustomerScoring::Analysis.query(income: 50000, zipcode: 60201, age: 35)).to be_a LjoyceCustomerScoring::Analysis
+    expect(LjoyceCustomerScoring::Analysis.query(
+      income: 50000, zipcode: 60201, age: 35)).to be_a LjoyceCustomerScoring::Analysis
   end
 
   # query method returns "Invalid query." for invalid query parameters
   it "fails validation with invalid query parameters, expect to return message \'Invalid query.\'" do
-    expect(LjoyceCustomerScoring::Analysis.query(income: 50000, zipcode: 60201)).to include("Invalid query.")
+    expect(LjoyceCustomerScoring::Analysis.query(
+      income: 50000, zipcode: 60201)).to include("Invalid query.")
   end
 
-  # @propensity is a 5-digit decimal betwee 0 and 1
+  # @propensity is a value between 0 and 1
+  it "should return a propensity between 0 and 1" do 
+    expect(LjoyceCustomerScoring::Analysis.query(
+      income: 50000, zipcode: 60201, age: 35).propensity).to be_between(0, 1)
+  end
+
+  # @propensity is a value  with 5-digit decimal
+  it "should return a propensity with 5 decimal places" do
+    expect(LjoyceCustomerScoring::Analysis.query(
+      income: 50000, zipcode: 60201, age: 35).
+      propensity.to_s.split('.').last.size).to eq(5)
+  end
 
   # @ranking is included in ["A", "B", "C", "D", "E"]
+  it "should return a ranking as a letter A–F" do
+    expect(["A", "B", "C", "D", "E", "F"]).to include(
+      LjoyceCustomerScoring::Analysis.query(
+      income: 50000, zipcode: 60201, age: 35).ranking)
+  end
 end
